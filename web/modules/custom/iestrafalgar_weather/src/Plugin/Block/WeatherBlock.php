1ab9d6f369831134e909ac897895bee2569607ac
jjunlob074 <jjunlob074@g.educaand.es>
2023-05-26 14:30:15 +0200

    comentarioff66

--- web/modules/custom/iestrafalgar_weather/src/Plugin/Block/WeatherBlock.php
+++ web/modules/custom/iestrafalgar_weather/src/Plugin/Block/WeatherBlock.php
@@ -21,7 +21,7 @@ class WeatherBlock extends BlockBase implements ContainerFactoryPluginInterface 
   protected $openApiConnector;
 
   /**
-   * Constructs a new WeatherBlock instance.
+   * Constructs a new WeatherBlock instances.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
